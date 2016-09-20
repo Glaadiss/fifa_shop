424abe88ef77ac7d2cac973e3eb14dd39baed81d
Glaadiss <bartekgladys@gmail.com>
2016-09-20 21:18:32 +0200

    finish

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -20,7 +20,7 @@ class AccountsController < ApplicationController
     pl_keys = ["id", "email", "facebook lub skype", "typ konsoli", "email konsoli", "hasło konsoli", "data urodzenia na konsoli", "web-app email",
               "hasło web-app", "odpowiedź na pytanie web-app", "odpowiedź na pytanie origin", "email origin", "origin hasło", "metoda płatności",
               "email do płatnośći", "confirmed", "failures", "token", "user_id", "language", "created_at", "updated_at"]
-    new_fields = params[:new_fields].each_slice(2).map{|k| [k[0] + k[1]]}.join(' | ')
+    new_fields = params[:new_fields].each_slice(2).map{|k| [k[0] + k[1]] }.join(' | ')
     @account = Account.find_by_token(params[:account_id])
     keys = @account.attributes.keys
     pl_hash = Hash[keys.zip(pl_keys)]
