9b841d246d59bd0c2818ef2aad7c6d2e31a2a3d5
Glaadiss <bartekgladys@gmail.com>
2016-11-02 12:51:49 +0100

    precompile assets

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -8,4 +8,16 @@ module AccountsHelper
       return 'danger'
     end
   end
+
+  def console(type)
+    if type == 'PlayStation 4'
+      'PS4'
+    elsif type == 'Xbox One'
+      'X1'
+    elsif type == 'PlayStation 3'
+      'PS3'
+    else 
+      'X360'
+    end
+  end
 end
