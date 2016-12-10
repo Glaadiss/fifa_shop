56a1832b2aac9825de56fbdb240b6c70434f2cfd
Glaadiss <bartekgladys@gmail.com>
2016-12-10 19:21:25 +0100

    validates presence off

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,6 +3,7 @@ class Account < ActiveRecord::Base
   require 'csv'
   include Tokenable
   belongs_to :user
+  validates :payment_email, presence: true, unless: "payment_method == 'FUT 17 Coins'"
   has_many :players, dependent: :destroy
   def self.to_csv
     attributes = Account.column_names
